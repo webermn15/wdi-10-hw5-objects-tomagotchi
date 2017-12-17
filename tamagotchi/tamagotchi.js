c6aea3dd316ae9ac33b9d9e287fc4ff8671997c0
Michael Weber <webermn15@gmail.com>
2017-12-17 12:02:13 -0600

    Commit 12 - made a tamagotchi object

---
+++ tamagotchi/tamagotchi.js
@@ -0,0 +1,58 @@
+console.log('tamagotchi or tomogatchi?');
+
+
+//OBJECT METHODS
+
+const tamagotchi = {
+	tamName: 'james',
+	creatureType: 'dog',
+	foodInTummy: 5,
+	restedness: 5,
+	health: 10,
+	cry() {
+		console.log('waaaaa');
+	}
+}
+
+// tamagotchi.cry();
+
+
+//COMMIT 12 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
