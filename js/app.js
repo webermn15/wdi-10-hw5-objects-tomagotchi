5fbed942d1acbaf9f1ff295e85056686895489c1
Michael Weber <webermn15@gmail.com>
2017-12-16 18:21:52 -0600

    Commit 2 - it's all about me done

--- js/app.js
+++ js/app.js
@@ -12,3 +12,29 @@ const clicker = {
 // clicker.clickCounter();
 
 // console.log(clicker.clickCount);
+
+
+//COMMIT 1
+
+
+const myInformation = {
+	myName: 'Michael',
+	myAge: 29,
+	myFact: 'I never learned how to read',
+	statement() {
+		return 'Hi, my name is '+this.myName+', in 10 years I\'ll be '+(this.myAge+10)+', and '+this.myFact+'.';
+	},
+	birthday() {
+		this.myAge++;
+	},
+	changeName(str) {
+		this.myName = str;
+	}
+}
+
+myInformation.birthday();
+
+myInformation.changeName('rumpelstiltskin');
+
+console.log(myInformation.statement());
+
