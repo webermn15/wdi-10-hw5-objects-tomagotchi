bba7892ba5177814e20e22151a9593a0424bb835
Michael Weber <webermn15@gmail.com>
2017-12-16 19:18:19 -0600

    Commit 8 - forEach done

---
+++ iterator_answers.js
@@ -0,0 +1,15 @@
+
+
+//Iterator answers
+
+const words = ["Ho", "Chi", "Minh", "City", "was", "once", "known", "as", "Prey", "Nokor"];
+
+words.forEach((foo) => {
+	foo.toUpperCase();	
+	// console.log(foo);
+})
+
+
+//COMMIT 8
+
+
